ffe4cd78f4856dd2d7ede02241ba80b8c89f19bc
ysnbk <130864850+ysnbk@users.noreply.github.com>
2025-09-26 23:50:39 +0000

    task 3

--- alx-project-0x02/components/common/PostModal.tsx
+++ alx-project-0x02/components/common/PostModal.tsx
@@ -2,6 +2,10 @@ const PostModal: React.FC = () =>{
   return (
    <div>
    <h1>Post Modal</h1>
+   <form action="">
+    <input type="text" name="title" placeholder="title" />
+    <textarea name="content" ></textarea>
+   </form>
    </div>
   );
 }
