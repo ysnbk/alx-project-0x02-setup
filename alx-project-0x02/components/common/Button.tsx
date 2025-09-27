d0511eeda88d7fb971c7e0d23acb32a8637c30b0
ysnbk <130864850+ysnbk@users.noreply.github.com>
2025-09-27 00:08:40 +0000

    task 4

---
+++ alx-project-0x02/components/common/Button.tsx
@@ -0,0 +1,9 @@
+import { ButtonProps } from "@/interfaces"
+ const Card: React.FC<ButtonProps> = ({size ,shape}) =>{
+  return (
+   <button className={[size,shape]}>
+   
+   </button>
+  );
+}
+export default Card
--- alx-project-0x02/interfaces/index.ts
+++ alx-project-0x02/interfaces/index.ts
@@ -2,3 +2,7 @@ export interface CardProps {
     title: string;
     content: string
 }
+export interface ButtonProps {
+    size: "small" | "medium" | "large";
+    shape: 'rounded-sm' | 'rounded-md' | 'rounded-full';
+}
